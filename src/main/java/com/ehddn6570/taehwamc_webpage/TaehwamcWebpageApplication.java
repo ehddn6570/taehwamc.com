96b55e10fdb1179b168cdcd9441a2c4934231706
ehddn6570 <ehddn6570@gmail.com>
2024-09-05 00:11:56 +0900

    init

---
+++ src/main/java/com/ehddn6570/taehwamc_webpage/TaehwamcWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.ehddn6570.taehwamc_webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaehwamcWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaehwamcWebpageApplication.class, args);
+	}
+
+}
